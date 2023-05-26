2fa27629b9d15bd467217db80f68d8f9dabd9b2b
plombir1917 <dfhhggh@list.ru>
2023-05-27 02:49:53 +0400

    night coding

---
+++ pages/chat.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <SideBar />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="imgWrapper">
+    <img src="../static/home.jpg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style scoped>
+.imgWrapper {
+  display: inline-block;
+  height: 100%;
+  vertical-align: top;
+  width: 100%;
+  height: 70%;
+  top: 0;
+}
+
+.imgWrapper > img {
+  width: 100%;
+  height: 70%;
+}
+</style>
